ac2951f772e178208d41070ca1f31834c5e3373e
Felipe <felipealves1995@gmail.com>
2022-09-23 20:25:33 -0300

    Melhorando o Script

--- IaC-ServerApache.sh
+++ IaC-ServerApache.sh
@@ -14,9 +14,7 @@ echo "Baixando os arquivos do Servidor Web"
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
+
 cd linux-site-dio-main
 cp -R * /var/www/html/
-
-
-
 
