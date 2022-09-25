cce0cba6c6a256163d532c1ba039f27b41909d33
Felipe <felipealves1995@gmail.com>
2022-09-25 14:40:04 -0300

    script para automatizar conexão SSH

---
+++ scriptssh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SERVIDORES=(
+  "172.22.228.122"
+  
+)
+
+if [ -z "$1" ]; then
+  echo "Você tem que digitar uma senha"
+  echo " ./scriptssh.sh 'senha'"
+  exit
+fi
+
+export SSHPASS=$1
+ 
+for SERVIDOR in "${SERVIDORES[@]}"
+do
+
+  echo $SERVIDOR
+   
+  sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $USER@$SERVIDOR || echo "FALHOU"
+   
+  ssh $USER@$SERVIDOR 'rm -rf .ssh'
+   
+  sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $USER@$SERVIDOR || echo "FALHOU"
+done
