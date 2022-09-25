f2d98c67eec73a95e1b2c08e8f76e78a04de8b0d
Felipe <felipealves1995@gmail.com>
2022-09-25 14:43:14 -0300

    Listar Container ativos

---
+++ ListContainerOn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Atualizando o Servidor"
+apt-get update && apt-get upgrade -y && apt-get install curl -y 
+
+echo "Puxando Container ativos"
+curl --unix-socket /var/run/docker.sock http://localhost/containers/json >> Container_Ativos.txt
+
