998da57944a7f31e786e882837c5185f68958db5
Felipe <felipealves1995@gmail.com>
2022-09-27 01:42:35 -0300

    Acrescentando coisas que faltaram no script.

--- Script_HD.sh
+++ Script_HD.sh
@@ -3,15 +3,25 @@
 
 echo "----------------------------------"
 echo "Executando SCRIPT...."
+#1º Atualizando o Linux e instalando o pacote de e-mail, caso não tenha
+#2ºNão recomendado pois fica mais lento o script!!
+#É necessario configurar as variaveis do pacote "mailto" para que o e-mail envie corretamente.
+apt-get update && apt-get upgrade -y && apt-get install mailto -y
+echo "----------------------------------"
+echo "----------------------------------"
+#Definindo os paremetros
 alerta_percente=80 # Percentual de alerta a ser verificado
 email_devOps="felipealves1995@gmail.com" # Coloca o e-mail do Analista da Infra
+#Excluindo partições que não nos servira
+echo "----------------------------------"
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read -r HD;
+echo "----------------------------------"
 do
   echo "$HD"
-  usep=$(echo "$HD" | awk '{ print $1}' | cut -d'%' -f1 )
-  partition=$(echo "$HD" | awk '{ print $2 }' )
-  if [ $usep -ge $ALERT ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-    mail -s "Alert: Almost out of disk space $usep%" "$email_devOps"
+  hd_uso=$(echo "$HD" | awk '{ print $1}' | cut -d'%' -f1 )
+  volumes=$(echo "$HD" | awk '{ print $2 }' )
+  if [ $hd_uso -ge $alerta_percente ]; then
+    echo  "Ficando sem espaço \" $volumes ( $hd_uso %) \" em $(hostname) como em $(date) "  |  
+    mail -s  "Alerta: Quase sem espaço em disco $hd_uso "  " $email_devOps "  
   fi
 done
