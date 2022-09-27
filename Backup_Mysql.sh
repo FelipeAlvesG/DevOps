a1ac40df0e3e3d096ebba4813b71dd6dac09875d
Felipe <felipealves1995@gmail.com>
2022-09-26 22:40:27 -0300

    Script para Backup do MySQL

---
+++ Backup_Mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Só de teste, simples, da para colocar para salvar com data, escolher outros locais, automatizar com horarios, etc.....
+#Definindo as variaveis, pode colocar em outro arquivo para ficar mais seguro.
+HOST="192.168.50.250"
+LOGIN="user"
+SENHA="senha123"
+BANCO="db_teste"
+
+#comando para executar o backup, assim vai tudo trigers, gatilhos, tabelas, etc...
+
+echo "Executando backup......"
+mysqldump -h $HOST -u $LOGIN -p$SENHA --opt --routines --triggers --default-character-set=utf8 $BANCO > backup_banco.sql
+
+echo "Finalizando backup do Banco $BANCO"
